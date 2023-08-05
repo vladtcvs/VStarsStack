6925beb95c5465e303c41f524c9eb2e7a584793c
Vladislav Tsendrovskii <vtcendrovskii@gmail.com>
2023-08-05 13:04:30 +0500

    Work on fine_shift

---
+++ src/vstarstack/tool/fine_shift/config.py
@@ -0,0 +1,26 @@
+#
+# Copyright (c) 2023 Vladislav Tsendrovskii
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+#
+
+import vstarstack.tool.configuration
+
+_module_configuration = {
+    "Nsteps" : (int, 2000),
+    "dh" : (float, 0.1),
+    "gridW" : (int, 64),
+    "gridH" : (int, 64),
+    "stretchPenaltyCoefficient" : (float, 0.01),
+    "points_min_len" : (int, 0),
+}
+
+configuration = vstarstack.tool.configuration.Configuration(_module_configuration)
