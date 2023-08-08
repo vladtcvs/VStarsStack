851d08d012e669a4b604e26fc667fedf8d2991e6
Vladislav Tsendrovskii <vtcendrovskii@gmail.com>
2023-08-08 12:11:17 +0500

    Deconvolution

--- src/vstarstack/tool/image_processing/deconvolution.py
+++ src/vstarstack/tool/image_processing/deconvolution.py
@@ -13,7 +13,6 @@
 #
 
 import os
-import cv2
 import numpy as np
 import multiprocessing as mp
 from skimage.restoration import richardson_lucy
@@ -22,44 +21,57 @@ import vstarstack.tool.common
 import vstarstack.tool.cfg
 import vstarstack.library.data
 
-def deconvolution(df : vstarstack.library.data.DataFrame, radius : int):
-    psf = np.zeros((2*radius+1, 2*radius+1))
-    cv2.circle(psf, (radius, radius), radius, 1, -1)
-    psf = psf / sum(psf)
+def deconvolution(df : vstarstack.library.data.DataFrame, strength : int):
+    psf = np.zeros((3, 3))
+
+    psf[1, 1] = 1
+
+    psf[0, 1] = 1/strength
+    psf[2, 1] = 1/strength
+    psf[1, 0] = 1/strength
+    psf[1, 2] = 1/strength
+
+    psf[0, 0] = 1/strength/1.41
+    psf[0, 2] = 1/strength/1.41
+    psf[2, 0] = 1/strength/1.41
+    psf[2, 0] = 1/strength/1.41
+
+    psf = psf / np.sum(psf)
+
     for channel in df.get_channels():
         image, opts = df.get_channel(channel)
         if not opts["brightness"]:
             continue
         norm = np.amax(image)
-        deconvolved_RL = richardson_lucy(image/norm, psf, num_iter=30)*norm
+        deconvolved_RL = richardson_lucy(image/norm, psf, num_iter=100)*norm
         deconvolved_RL[np.where(np.isnan(deconvolved_RL))] = 0
         df.replace_channel(deconvolved_RL, channel)
     return df
 
-def _process_file(input : str, output : str, radius : int):
+def _process_file(input : str, output : str, strength : int):
     df = vstarstack.library.data.DataFrame.load(input)
-    deconvolution(df, radius)
+    deconvolution(df, strength)
     df.store(output)
 
-def _process_dir(inputs : str, outputs : str, radius : int):
+def _process_dir(inputs : str, outputs : str, strength : int):
     files = vstarstack.tool.common.listfiles(inputs, ".zip")
     with mp.Pool(vstarstack.tool.cfg.nthreads) as pool:
         pool.starmap(_process_file, [(fname,
                                       os.path.join(outputs, name + ".zip"),
-                                      radius) for name, fname in files])
+                                      strength) for name, fname in files])
 
 def run(project : vstarstack.tool.cfg.Project, argv : list[str]):
     """Deconvolution"""
     if len(argv) >= 3:
         inputs = argv[0]
-        radius = int(argv[1])
+        strength = int(argv[1])
         outputs = argv[2]
     else:
         inputs = project.config.paths.npy_fixed
         outputs = project.config.paths.npy_fixed
-        radius = int(argv[0])
+        strength = int(argv[0])
 
     if os.path.isdir(inputs):
-        _process_dir(inputs, outputs, radius)
+        _process_dir(inputs, outputs, strength)
     else:
-        _process_file(inputs, outputs, radius)
+        _process_file(inputs, outputs, strength)
