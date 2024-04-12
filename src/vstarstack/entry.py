3a0fd03869f6bd3b94d24acd0cd4d48f258ad3ee
Vladislav Tsendrovskii <vtcendrovskii@gmail.com>
2024-04-12 05:48:10 +0500

    Use console_scripts instead of scripts

---
+++ src/vstarstack/entry.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import sys
+
+import vstarstack.tool.process
+import vstarstack.tool.cfg
+import vstarstack.tool.usage
+
+def main():
+    program_project = vstarstack.tool.cfg.get_project()
+
+    if vstarstack.tool.cfg.get_param("autocomplete", bool, False):
+        #with open("complete", "w") as f:
+        #    print(sys.argv, file=f)
+        program_argv = [item for item in sys.argv[2:] if item[:2] != "--"]
+        variants = vstarstack.tool.usage.autocompletion(vstarstack.tool.process.commands,
+                                                    program_argv)
+        for variant in variants:
+            print(variant)
+    else:
+        program_argv = [item for item in sys.argv[1:] if item[:2] != "--"]
+        vstarstack.tool.usage.run(program_project,
+                                  program_argv,
+                                  "",
+                                  vstarstack.tool.process.commands)
