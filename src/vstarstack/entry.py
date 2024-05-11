0bd6000eba6e85c4c1eefdbe89ad81f97e3ab838
Vladislav Tsendrovskii <vtcendrovskii@gmail.com>
2024-05-11 14:18:18 +0500

    Add copyrights

--- src/vstarstack/entry.py
+++ src/vstarstack/entry.py
@@ -1,5 +1,20 @@
 #!/usr/bin/env python3
 
+#
+# Copyright (c) 2024 Vladislav Tsendrovskii
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+#
+
+
 import sys
 
 import vstarstack.tool.process
