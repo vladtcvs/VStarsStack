Fix normalization and merging (#134)
* Make normalization work more correct
* Clarify code of kappa sigma

src/vstarstack/library/merge/simple_add.py
@@ -14,6 +14,8 @@
 
 import numpy as np
 import vstarstack.library.data
+import vstarstack.library.common
+import vstarstack.library.image_process.normalize
 from vstarstack.library.data import DataFrame
 from copy import deepcopy
 
@@ -30,24 +32,24 @@ def simple_add(images : vstarstack.library.common.IImageSource) -> DataFrame:
     channel_opts = {}
     for img in images.items():
         params = img.params
+        img = vstarstack.library.image_process.normalize.denormalize(img)
         for channel_name in img.get_channels():
             channel, opts = img.get_channel(channel_name)
-            if not opts["brightness"]:
+            if not img.get_channel_option(channel_name, "brightness"):
                 continue
             if channel_name not in channel_opts:
                 channel_opts[channel_name] = opts
 
-            if channel_name in img.links["weight"]:
-                weight_channel = img.links["weight"][channel_name]
-                weight, _ = img.get_channel(weight_channel)
-            else:
-                weight = np.ones(channel.shape, dtype=np.float64)
+            weight, _, _ = img.get_linked_channel(channel_name, "weight")
+            if weight is None:
+                if (weight_k := img.get_parameter("weight")) is None:
+                    weight_k = 1
+                weight = np.ones(channel.shape, dtype=np.float64) * weight_k
 
             if channel_name not in summary:
                 summary[channel_name] = deepcopy(channel.astype(np.float64))
                 summary_weight[channel_name] = deepcopy(weight)
             else:
-
                 try:
                     summary[channel_name] += channel
                     summary_weight[channel_name] += weight
@@ -57,9 +59,9 @@ def simple_add(images : vstarstack.library.common.IImageSource) -> DataFrame:
     result = vstarstack.library.data.DataFrame(params=params)
     for channel_name, channel in summary.items():
         print(channel_name)
+        weight_channel_name = "weight-"+channel_name
         result.add_channel(channel, channel_name, **channel_opts[channel_name])
-        result.add_channel(summary_weight[channel_name],
-                           "weight-"+channel_name, weight=True)
-        result.add_channel_link(channel_name, "weight-"+channel_name, "weight")
+        result.add_channel(summary_weight[channel_name], weight_channel_name, weight=True)
+        result.add_channel_link(channel_name, weight_channel_name, "weight")
 
     return result