7d2d1cde324684b8065325e22cdc0c208627349a
Vladislav Tsendrovskii <vtcendrovskii@gmail.com>
2024-09-10 03:37:33 +0200

    Implement 'mask' method

--- src/vstarstack/library/debayer/bayer.py
+++ src/vstarstack/library/debayer/bayer.py
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2022 Vladislav Tsendrovskii
+# Copyright (c) 2022-2024 Vladislav Tsendrovskii
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -13,7 +13,21 @@
 #
 
 import numpy as np
+from enum import Enum
+
 import vstarstack.library.data
+
+class DebayerMethod(Enum):
+    """
+    Debayer methods:
+
+        SUBSAMPLE - convert each 2*2 pixels square to 1 colour pixel
+        MASK - separate image to color layers according to mask, and layer has weight=0 in points of other colors
+        INTERPOLATE - interpolate missing colors of each pixel from neighbours
+    """
+    SUBSAMPLE = 0,
+    MASK = 1,
+    INTERPOLATE = 2,
 
 def generate_mask(name):
     """Generate mask by name"""
@@ -30,13 +44,12 @@ def generate_mask(name):
     result_mask = {}
     for color in used_colors:
         color_mask = np.array(mask[color])
-        color_mask = color_mask / np.sum(color_mask)
         result_mask[color] = color_mask
     return result_mask
 
-def debayer_image(image : np.ndarray,
-                  weight : float,
-                  mask : dict):
+def debayer_image_subsample(image : np.ndarray,
+                            weight : np.ndarray,
+                            mask : dict):
     """Process debayer on image"""
     h = image.shape[0]
     w = image.shape[1]
@@ -57,19 +70,76 @@ def debayer_image(image : np.ndarray,
         w -= 1
 
     for color in mask:
-        k00 = mask[color][0,0]
-        k01 = mask[color][0,1]
-        k10 = mask[color][1,0]
-        k11 = mask[color][1,1]
+        k00 = mask[color][0,0]/np.sum(mask[color])
+        k01 = mask[color][0,1]/np.sum(mask[color])
+        k10 = mask[color][1,0]/np.sum(mask[color])
+        k11 = mask[color][1,1]/np.sum(mask[color])
+
         image00 = image[0::2, 0::2]
         image01 = image[0::2, 1::2]
         image10 = image[1::2, 0::2]
         image11 = image[1::2, 1::2]
-        weight00 = np.ones(image00.shape)*weight
-        weight01 = np.ones(image01.shape)*weight
-        weight10 = np.ones(image10.shape)*weight
-        weight11 = np.ones(image11.shape)*weight
 
+        weight00 = weight[0::2, 0::2]
+        weight01 = weight[0::2, 1::2]
+        weight10 = weight[1::2, 0::2]
+        weight11 = weight[1::2, 1::2]
+
+        layers[color] = k00 * image00 + k01 * image01 + k10 * image10 + k11 * image11
+        weights[color] = k00 * weight00 + k01 * weight01 + k10 * weight10 + k11 * weight11
+
+    return layers, weights
+
+def debayer_image_mask(image : np.ndarray,
+                       weight : np.ndarray,
+                       mask : dict):
+    """Process debayer on image"""
+
+    image00 = np.copy(image)
+    weight00 = np.copy(image)
+    image00[0::2, 1::2] = 0
+    weight00[0::2, 1::2] = 0
+    image00[1::2, 0::2] = 0
+    weight00[1::2, 0::2] = 0
+    image00[1::2, 1::2] = 0
+    weight00[1::2, 1::2] = 0
+
+    image01 = np.copy(image)
+    weight01 = np.copy(image)
+    image01[0::2, 0::2] = 0
+    weight01[0::2, 0::2] = 0
+    image01[1::2, 0::2] = 0
+    weight01[1::2, 0::2] = 0
+    image01[1::2, 1::2] = 0
+    weight01[1::2, 1::2] = 0
+
+    image10 = np.copy(image)
+    weight10 = np.copy(image)
+    image10[0::2, 0::2] = 0
+    weight10[0::2, 0::2] = 0
+    image10[0::2, 1::2] = 0
+    weight10[0::2, 1::2] = 0
+    image10[1::2, 1::2] = 0
+    weight10[1::2, 1::2] = 0
+
+    image11 = np.copy(image)
+    weight11 = np.copy(image)
+    image11[0::2, 0::2] = 0
+    weight11[0::2, 0::2] = 0
+    image11[0::2, 1::2] = 0
+    weight11[0::2, 1::2] = 0
+    image11[1::2, 0::2] = 0
+    weight11[1::2, 0::2] = 0
+
+    layers = {}
+    weights = {}
+
+    for color in mask:
+        k00 = mask[color][0,0]
+        k01 = mask[color][0,1]
+        k10 = mask[color][1,0]
+        k11 = mask[color][1,1]
+        
         layers[color] = k00 * image00 + k01 * image01 + k10 * image10 + k11 * image11
         weights[color] = k00 * weight00 + k01 * weight01 + k10 * weight10 + k11 * weight11
 
@@ -77,13 +147,23 @@ def debayer_image(image : np.ndarray,
 
 def debayer_dataframe(dataframe : vstarstack.library.data.DataFrame,
                       mask : dict,
-                      raw_channel_name : str):
+                      raw_channel_name : str,
+                      method : DebayerMethod):
     """Debayer dataframe"""
     raw, _ = dataframe.get_channel(raw_channel_name)
-    if (weight := dataframe.get_parameter("weight")) is None:
-        weight = 1
+    weight, _, _ = dataframe.get_linked_channel(raw_channel_name, "weight")
+    if weight is None:
+        if (weight_val := dataframe.get_parameter("weight")) is None:
+            weight_val = 1
+        weight = np.ones(raw.shape)*weight_val
 
-    layers, weights = debayer_image(raw, weight, mask)
+    if method == DebayerMethod.SUBSAMPLE:
+        layers, weights = debayer_image_subsample(raw, weight, mask)
+    elif method == DebayerMethod.MASK:
+        layers, weights = debayer_image_mask(raw, weight, mask)
+    elif method == DebayerMethod.INTERPOLATE:
+        raise NotImplemented()
+
     for color in layers:
         dataframe.add_channel(layers[color], color, brightness=True, signal=True)
         dataframe.add_channel(weights[color], f"weight-{color}", weight=True)
@@ -93,10 +173,12 @@ def debayer_dataframe(dataframe : vstarstack.library.data.DataFrame,
         dataframe.remove_channel(dataframe.links["weight"][raw_channel_name])
     dataframe.remove_channel(raw_channel_name)
 
-    dataframe.add_parameter(int(dataframe.get_parameter("w")/2), "w")
-    dataframe.add_parameter(int(dataframe.get_parameter("h")/2), "h")
-    dataframe.add_parameter("flat", "format")
-    if dataframe.get_parameter("projection") == "perspective":
-        dataframe.params["projection_perspective_kw"] *= 2
-        dataframe.params["projection_perspective_kh"] *= 2
+    if method == DebayerMethod.SUBSAMPLE:
+        dataframe.add_parameter(int(dataframe.get_parameter("w")/2), "w")
+        dataframe.add_parameter(int(dataframe.get_parameter("h")/2), "h")
+        dataframe.add_parameter("flat", "format")
+        if dataframe.get_parameter("projection") == "perspective":
+            dataframe.params["projection_perspective_kw"] *= 2
+            dataframe.params["projection_perspective_kh"] *= 2
+
     return dataframe
