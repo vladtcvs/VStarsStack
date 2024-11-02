802b72d5ab30df7c002b542d21439a7e834dddd8
Vladislav Tsendrovskii <vtcendrovskii@gmail.com>
2024-11-02 08:40:34 +0100

    Add LC tests

--- tests/test_fine_shift.py
+++ tests/test_fine_shift.py
@@ -232,6 +232,32 @@ def test_approximate_by_correlation1():
     assert np.amin(data) == 0
     assert np.amax(data) == 0
 
+def test_approximate_by_correlation2():
+    df1 = next(readjpeg(os.path.join(dir_path, "fine_shift/image3.png")))
+    df2 = next(readjpeg(os.path.join(dir_path, "fine_shift/image4.png")))
+
+    image = df1.get_channel("L")[0].astype('double')
+    image_ref = df2.get_channel("L")[0].astype('double')
+
+    w = image.shape[1]
+    h = image.shape[0]
+
+    grid = ImageGrid(image_w=w, image_h=h)
+    grid.fill(image)
+    grid_ref = ImageGrid(image_w=w, image_h=h)
+    grid_ref.fill(image_ref)
+
+    lc = ImageDeformLC(image_w=w, image_h=h, pixels=1)
+    deform = lc.find(grid, None, grid_ref, None, 5, 5, 1)
+    assert deform is not None
+
+    data = deform.content()
+    assert data.shape[0] == h
+    assert data.shape[1] == w
+    assert data.shape[2] == 2
+    assert np.amin(data) >= 0
+    assert np.amax(data) == 1
+
 def test_memory_leak():
     N = 5
     df1 = next(readjpeg(os.path.join(dir_path, "fine_shift/image1.png")))
