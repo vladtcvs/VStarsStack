637f16c5eb08430a0f9b8536e2d0b47b7301fcc4
Vladislav Tsendrovskii <vtcendrovskii@gmail.com>
2025-06-09 02:33:09 +0200

    fix

--- src/vstarstack/library/bayes/aposteriori.py
+++ src/vstarstack/library/bayes/aposteriori.py
@@ -13,15 +13,14 @@
 # along with this program. If not, see <https://www.gnu.org/licenses/>.
 #
 
+from typing import Callable
 import numpy as np
 
 def posterior_item(F : np.ndarray,
                    f_posteriori : np.ndarray,
                    f_integration : np.ndarray,
                    lambdas_d : np.ndarray,
-                   lambdas_v : np.ndarray,
-                   apriori_f_posterior : float,
-                   apriori_f_integration : float) -> float:
+                   lambdas_v : np.ndarray) -> float:
 
     lambdas_f_posterior   = lambdas_d + np.matmul(lambdas_v, f_posteriori)
     Lambdas_f_posterior   = np.sum(F * np.log(lambdas_f_posterior)   - lambdas_f_posterior)
@@ -29,28 +28,11 @@ def posterior_item(F : np.ndarray,
     lambdas_f_integration = lambdas_d + np.matmul(lambdas_v, f_integration)    
     Lambdas_f_integration = np.sum(F * np.log(lambdas_f_integration) - lambdas_f_integration)
 
-    return np.exp(Lambdas_f_integration - Lambdas_f_posterior) * apriori_f_integration / apriori_f_posterior
+    return np.exp(Lambdas_f_integration - Lambdas_f_posterior)
 
-def _find_apriori(apriory_table : np.ndarray, indexes : np.ndarray)->float:
-    """Apriori probability p(f_intergation)"""
-    return apriory_table[tuple(indexes)]
-
-def _posterior_item(F : np.ndarray,
-                    f_posteriori : np.ndarray,
-                    lambdas_d : np.ndarray,
-                    lambdas_v : np.ndarray,
-                    apriori_table : np.ndarray,
-                    apriori_f_posterior : float,
-                    indexes_integration : np.ndarray,
-                    limits_low : np.ndarray,
-                    dl : float) -> float:
-    f_integration : np.ndarray = limits_low + indexes_integration * dl
-    apriori_f_integration : float = _find_apriori(apriori_table, indexes_integration)
-    if apriori_f_integration == 0:
-        return 0
-    return posterior_item(F, f_posteriori, f_integration, lambdas_d, lambdas_v, apriori_f_posterior, apriori_f_integration)
-
-def _get_indexes(limits_low : np.ndarray, num_indexes : np.ndarray, dl : float):
+def _get_indexes(limits_low : np.ndarray, limits_high : np.ndarray, dl : float):
+    num_indexes = np.ceil((limits_high - limits_low)/dl).astype(np.uint)
+    num_indexes = np.clip(num_indexes, 1, None)
     dims = limits_low.shape[0]
     index = np.zeros((dims))
     while True:
@@ -64,29 +46,31 @@ def _get_indexes(limits_low : np.ndarray, num_indexes : np.ndarray, dl : float):
             break
 
 def find_posterior(F : np.ndarray,
-                   indexes_posteriori : np.ndarray,
+                   f_posteriori : np.ndarray,
                    lambdas_d : np.ndarray,
                    lambdas_v : np.ndarray,
-                   apriori_table : np.ndarray,
+                   apriori : Callable[[np.ndarray], float],
                    limits_low : np.ndarray,
-                   num_indexes : np.ndarray,
+                   limits_high : np.ndarray,
                    dl : float) -> float:
     """
         Find p(f_posteriori | {F_i})
     """
-    assert lambdas_v.shape[1] == indexes_posteriori.shape[0]
+    assert lambdas_v.shape[1] == f_posteriori.shape[0]
     assert lambdas_d.shape[0] == lambdas_v.shape[0]
     assert lambdas_v.shape[0] == F.shape[0]
-    assert limits_low.shape[0] == indexes_posteriori.shape[0]
-    assert num_indexes.shape[0] == indexes_posteriori.shape[0]
+    assert limits_low.shape[0] == f_posteriori.shape[0]
+    assert limits_high.shape[0] == f_posteriori.shape[0]
 
-    f_posteriori : np.ndarray = limits_low + indexes_posteriori * dl
-
-    apriori_f_posterior : float = _find_apriori(apriori_table, f_posteriori)
+    apriori_f_posterior = apriori(f_posteriori)
     if apriori_f_posterior == 0:
         return 0
     s = 0
-    for indexes in _get_indexes(limits_low, num_indexes, dl):
-        item = _posterior_item(F, f_posteriori, lambdas_d, lambdas_v, apriori_table, apriori_f_posterior, indexes, limits_low, dl)
+    for indexes in _get_indexes(limits_low, limits_high, dl):
+        f_integration : np.ndarray = limits_low + indexes * dl
+        apriori_f_integration = apriori(f_integration)
+        if abs(apriori_f_integration) < 1e-12:
+            continue
+        item = posterior_item(F, f_posteriori, f_integration, lambdas_d, lambdas_v) * apriori_f_integration / apriori_f_posterior
         s += item * dl**len(indexes)
     return 1/s
