89ebd8baf3efbfb47c516b32ab56bf13a250ff11
Vladislav Tsendrovskii <vtcendrovskii@gmail.com>
2025-09-21 19:43:35 +0200

    Bump version 0.3.7

--- setup.py
+++ setup.py
@@ -76,7 +76,7 @@ print("Packages: ", result)
 packages = result
 
 setup (name = 'vstarstack',
-       version = '0.3.6',
+       version = '0.3.7',
        author='Vladislav Tsendrovskii',
        description = 'Stacking astrophotos',
        package_dir = {'vstarstack': 'src/vstarstack'},
